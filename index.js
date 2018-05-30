93ed40ea69d26cf78613c81a91345da18269dd5f
Bacra Woo <bacra.woo@gmail.com>
2018-05-30 10:38:52 +0800

    Update: split enter.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var base = require('./lib/base');
 var example = require('./lib/example');
+var enter = require('./lib/enter');
+var errorUtils = require('./lib/error');
 
 exports = module.exports = example.extend.bind(example);
 exports.Base = base.Base;
+
+// 入口方法，可以忽略Processor实例，进行使用者两个方法调用
+exports.connect = enter.connect;
+exports.shell = enter.shell;
+
+// 工具方法
+exports.throw = errorUtils.throw;
+exports.error = errorUtils.error;
