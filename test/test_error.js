d896824b4669d5079cb719f884e05bb40475475c
Bacra Woo <bacra.woo@gmail.com>
2018-05-30 10:13:42 +0800

    Update: error message

--- test/test_error.js
+++ test/test_error.js
@@ -6,7 +6,7 @@ describe('#err', function()
 	it('#new', function()
 	{
 		var err = errorUtils.error();
-		expect(err.message).to.be('connc');
+		expect(err.message.substr(0,6)).to.be('connc,');
 		// 第一行调用栈是当前文件
 		expect(err.stack.split(/\n/)[1].trim()).to.contain(__filename);
 	});
