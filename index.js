25af6c9b792f8295ce28a048d04e64789dd52faa
Bacra Woo <bacra.woo@gmail.com>
2018-06-26 20:12:02 +0800

    Update: clear hanlders

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 'use strict';
 
-var base = require('./lib/base');
 var example = require('./lib/example');
-var enter = require('./lib/func/enter');
 var errorUtils = require('./lib/func/error');
 
 exports = module.exports = example.extend.bind(example);
-exports.Base = base.Base;
-
-// 入口方法，可以忽略Processor实例，进行使用者两个方法调用
-exports.connect = enter.connect;
-exports.shell = enter.shell;
 
 // 工具方法
 exports.throw = errorUtils.throw;
